4b0d8e57ede5a8eba15439025be32dd28ad3d978
Andrei Scutariu <andrei.scutariu@endava.com>
2019-12-13 08:27:22 +0200

    delete not needed files

--- advanced.day.03.solid/SOLID and Other Principles/3. Liskov Substitution/VirtualCallInConstructor/DatabaseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SOLID_and_Other_Principles._3._Liskov_Substitution.VirtualCallInConstructor
-{
-    public class DatabaseRepository
-    {
-        public void Add()
-        {
-        }
-    }
-}
