7a9652bd4571f1e177c61be7eea2e37120158aca
Andrei Scutariu <andrei.scutariu@endava.com>
2019-12-17 09:31:55 +0200

    updates

---
+++ db.day.01/3.day6.efc/demo/db-first-wmig-simple/EfCoreSamples.DbFirst.DbRunner/Scripts/01.CreateOrderTable.sql
@@ -0,0 +1,14 @@
+﻿USE [StoreDB_3]
+GO
+
+CREATE TABLE [dbo].[Orders](
+	[OrderId] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerId] [int] NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+	[OrderDate] [datetime2](7) NOT NULL,
+	CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+	(
+		[OrderId] ASC
+	) ON [PRIMARY]
+) ON [PRIMARY]
+GO
