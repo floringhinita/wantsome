a7e9df7b44a15a1635d73c1f5ecd77802ca78ec7
Florin Ghinita <florin.ghinita@webbeds.com>
2020-01-09 09:50:25 +0200

    twitter tests

---
+++ advanced.day.03.unit.tests/twitter.xunit/MicrowaveOvenTests .cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace twitter.xunit
+{
+    public class MicrowaveOvenTests
+    {
+        [Fact]
+        public void Test_SendTweetToServerShouldSendTheMessageToItsServer()
+        {
+
+        }
+
+        [Fact]
+        public void Test_WriteTweetShouldCallItsWriterWithTheTweetsMessage()
+        {
+
+        }
+    }
+}
---
+++ advanced.day.03.unit.tests/twitter.xunit/TwitterAppTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace twitter.xunit
+{
+    public class TwitterAppTests
+    {
+        [Fact]
+        public void Test_SendTweetToServerShouldSendTheMessageToItsServer()
+        {
+
+        }
+
+    }
+}
